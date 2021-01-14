d87abed65292ef65ef507990d9cb028a89087137
DilanRamirez <drramirezra@miners.utep.edu>
2021-01-14 16:30:08 -0700

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["/"];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      // Open a cache and cache our files
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
